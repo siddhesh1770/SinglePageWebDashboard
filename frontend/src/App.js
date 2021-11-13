cb0ebe1021cbe6d697a3e6ae1b801daeeaf79b06
Siddhesh Patil <sidpatil1770@gmail.com>
2021-11-13 17:48:33 +0530

    messed up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,21 @@ import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import React from "react";
 import LandingPage from "./screens/LandingPage/LandingPage";
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MyBoard from "./screens/MyBoard/MyBoard";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
       <main>
-        <LandingPage />
+        <Routes>
+          <Route path="/" component={LandingPage} />
+          <Route path="/myboard" component={MyBoard} />
+        </Routes>
       </main>
-
       <Footer />
-    </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/screens/MyBoard/MyBoard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyBoard = () => {
+  return <h1>My Board</h1>;
+};
+
+export default MyBoard;
