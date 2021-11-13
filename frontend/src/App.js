62713fb2ed228c7af1ee3b01514eb77592862ee6
Siddhesh Patil <sidpatil1770@gmail.com>
2021-11-13 17:23:01 +0530

    landing page and header footer component created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import React from "react";
+import LandingPage from "./screens/LandingPage/LandingPage";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <div className="App">Hello World</div>
-      <div className="App">Hello World</div>
+      <Header />
+      <main>
+        <LandingPage />
+      </main>
+
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+const Footer = () => {
+  return (
+    <footer
+      style={{
+        width: "100%",
+        position: "relative",
+        bottom: 0,
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <Container>
+        <Row>
+          <Col className="text-center py-3">
+            Copyright &copy; Single Page Dashboard By Siddhesh Patil
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
